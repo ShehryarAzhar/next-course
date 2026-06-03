a5bbd4046f717ec2a4e73ce448a7ffd19f4f4e9a
ShehryarAzhar <m.shehryarazhar@outlook.com>
2026-06-03 19:04:53 +0500

    Database integration with Prisma

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,17 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { prisma } from "@/prisma/client";
 
 interface Props {
-  params: Promise<{ id: number }>;
+  params: Promise<{ id: string }>;
 }
 
 export async function GET(request: NextRequest, { params }: Props) {
   const { id } = await params;
 
-  if (id > 10)
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found." }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: "Shery" });
+  return NextResponse.json(user);
 }
 
 export async function PUT(request: NextRequest, { params }: Props) {
@@ -23,17 +30,38 @@ export async function PUT(request: NextRequest, { params }: Props) {
   if (!validation.success)
     return NextResponse.json(validation.error.issues, { status: 400 });
 
-  if (id > 10)
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: { name: body.name, email: body.email },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(request: NextRequest, { params }: Props) {
   const { id } = await params;
 
-  if (id > 10)
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  await prisma.user.delete({
+    where: { id: user.id },
+  });
 
   return NextResponse.json({});
 }
---
+++ prisma/client.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "pakistan1",
+  database: "nextapp",
+});
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
