04d30d778dca8ce2c56c134a228bd82411c61896
ShehryarAzhar <m.shehryarazhar@outlook.com>
2026-06-04 12:35:47 +0500

    Uploading Files

---
+++ app/upload/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="Hogwarts Student"
+        />
+      )}
+      <CldUploadWidget
+        uploadPreset="nextapp"
+        onSuccess={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
